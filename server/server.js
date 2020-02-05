f763e93a819788008bb359ccce596513200ba435
ColeHuntley83 <calrugby1@gmail.com>
2020-02-05 08:48:04 -0800

    fix

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const axios = require('axios');
+const express = require('express');
+const morgan = require('morgan');
+
+const port = 3000;
+let app = express();
+app.use(morgan('dev'));
+console.log(__dirname + '/../client')
+app.use(express.static(__dirname + '/../client'));
+app.use(express.json());
+
+app.listen(port, function(){
+console.log(`Coles PROXY server is listening on port ${port}`)
+});
