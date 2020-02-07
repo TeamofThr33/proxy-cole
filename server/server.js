d055c35c67381adf2de75996aa4478cb883a62c3
ColeHuntley83 <calrugby1@gmail.com>
2020-02-06 19:58:37 -0800

    cleanup

--- server/server.js
+++ server/server.js
@@ -5,10 +5,11 @@ const morgan = require('morgan');
 const port = 3000;
 let app = express();
 app.use(morgan('dev'));
-console.log(__dirname + '/../client')
+
 app.use(express.static(__dirname + '/../client'));
 app.use(express.json());
 
 app.listen(port, function(){
 console.log(`Coles PROXY server is listening on port ${port}`)
 });
+
