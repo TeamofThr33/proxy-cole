3db701573b354eda4e060977fb2fb3c70be3d7d4
ColeHuntley83 <calrugby1@gmail.com>
2020-02-11 09:30:06 -0800

    new caching

--- server/server.js
+++ server/server.js
@@ -2,14 +2,23 @@ const axios = require('axios');
 const express = require('express');
 const morgan = require('morgan');
 const compression = require('compression');
+const responseTime = require('response-time');
+const redis = require('redis');
+const client = redis.createClient();
+
+
+
 
 const port = 3000;
 let app = express();
 app.use(morgan('dev'));
 app.use(compression());
+app.use(responseTime());
 
 app.use(express.static(__dirname + '/../client'));
 app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 
 //zacks component
@@ -50,16 +59,60 @@ app.get("/restaraunts/bundle.js", (req, res)=> {
 
 });
 
+checkCache2 = (req, res, next) => {
+  const id = "restaurants";
+  
+  //get data value for key =id
+  client.get(id, (err, data) => {
+      if (err) {
+          //console.log(err);
+          res.status(500).send(err);
+      }
+      //if no match found
+      if (data != null) {
+          console.log("using Restaurant cached data!!!!!!!!")
+          res.send(data);
+      } 
+      else {
+          //proceed to next middleware function
+          next();
+      }
+   });
+};
 
-app.get('/restaurants', (req, res) => {
-        axios.get('http://localhost:3001/restaurants')
-          .then((response) => {
-            res.status(200).send(response.data);
-          })
-          .catch((err) => {
-            res.status(500).end;
-          })
-      });
+app.get("/restaurants", checkCache2, async (req, res) => {
+  try {
+       const  id  = "restaurants";
+       
+       const restaurants = await axios.get(
+       `http://localhost:3001/restaurants`
+       );
+       
+       //get data from response
+       const restaurantData = restaurants.data;
+       
+       client.setex(id, 100, JSON.stringify(restaurantData));
+       
+       return res.json(restaurantData);
+  } 
+  catch (error) {
+       
+       console.log(error);
+       return res.status(500).json(error);
+   }
+});
+
+
+
+// app.get('/restaurants', (req, res) => {
+//         axios.get('http://localhost:3001/restaurants')
+//           .then((response) => {
+//             res.status(200).send(response.data);
+//           })
+//           .catch((err) => {
+//             res.status(500).end;
+//           })
+//       });
 
 //       app.get(/icons/, function(req, res) {
 //         res.redirect('http://localhost:3001'+req.url);
@@ -80,15 +133,67 @@ app.get("/images/bundle.js", (req, res)=> {
 
 
 });
-app.get('/images', (req, res) => {
-        axios.get('http://localhost:3004/images')
-          .then((response) => {
-            res.status(200).send(response.data);
-          })
-          .catch((err) => {
-            res.status(500).end;
-          })
-      })
+
+//testing cache function
+checkCache = (req, res, next) => {
+  const id = "images";
+  
+  //get data value for key =id
+  client.get(id, (err, data) => {
+      if (err) {
+          //console.log(err);
+          res.status(500).send(err);
+      }
+      //if no match found
+      if (data != null) {
+          console.log("using image cached data!!!!!!!!")
+          res.send(data);
+      } 
+      else {
+          //proceed to next middleware function
+          next();
+      }
+   });
+};
+
+app.get("/images", checkCache, async (req, res) => {
+  try {
+       const  id  = "images";
+       
+       const images = await axios.get(
+       `http://localhost:3004/images`
+       );
+       
+       //get data from response
+       const imageData = images.data;
+       client.setex(id, 100, JSON.stringify(imageData));
+       
+       return res.json(imageData);
+  } 
+  catch (error) {
+       
+       console.log(error);
+       return res.status(500).json(error);
+   }
+});
+
+
+
+
+
+
+
+
+
+// app.get('/images', (req, res) => {
+//         axios.get('http://localhost:3004/images')
+//           .then((response) => {
+//             res.status(200).send(response.data);
+//           })
+//           .catch((err) => {
+//             res.status(500).end;
+//           })
+//       })
 
 
 // app.get(/icons/, function(req, res) {
